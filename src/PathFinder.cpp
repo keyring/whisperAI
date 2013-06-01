9f8766c30228383427acac443bce8541fc1b8d87
z.keyring <keyrings@163.com>
2013-06-01 15:36:48 +0800

    Add PathFinder.cpp

---
+++ src/PathFinder.cpp
@@ -0,0 +1,26 @@
+/*****************************************************************************
+ *
+ *    PathFinder.cpp by z.keyring (c) 2013 All Rights Reserved.
+ *
+ *                                    --code with Emacs :)
+ ****************************************************************************/
+
+#include <iostream>
+
+#include "PathFinder.cpp"
+
+using namespace std;
+
+extern const char *g_applicationName;
+extern const char *g_windowClassName;
+
+void PathFinder::CreateGraph( int cellsUp, int cellsAcross ){
+
+
+}
+
+void PathFinder::PointToIndex( POINTS p, int &nodeIndex ){
+
+
+}
+
