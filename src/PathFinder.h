86c4d3ee793393f97fc3d0475e35c0795c401bfb
z.keyring <keyrings@163.com>
2013-06-01 13:46:23 +0800

    Add PathFinder.h

---
+++ src/PathFinder.h
@@ -0,0 +1,30 @@
+/*****************************************************************************
+ *
+ *    PathFinder.h by z.keyring (c) 2013 All Rights Reserved.
+ *
+ *                                     --code with Emacs :)
+ ****************************************************************************/
+
+#ifndef __PATHFINDER_H__
+#define __PATHFINDER_H__
+
+#pragma warning (disable:4786)
+
+#include <windows.h>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <list>
+
+
+class PathFinder{
+
+ public :
+
+  enum BRUSH_TYPE{
+  };
+
+  enum ALGORITHM_TYPE{
+  };
+
+#endif	/* __PATHFINDER_H__ */
