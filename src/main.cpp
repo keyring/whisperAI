32af664d397e2f856510d4dca60ce35a41201b01
z.keyring <keyrings@163.com>
2013-06-01 12:40:17 +0800

    Init window function..

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+/*****************************************************************************
+ *
+ *    main.cpp by z.keyring (c) 2013 All Rights Reserved.
+ *
+ *                                  --code with Emacs :)
+ ****************************************************************************/
+
+#pragma warning ( disable:4786 )
+
+#include <windows.h>
+#include <time.h>
+
+const char *g_applicationName = "Path Finder";
+const char *g_windowClassName = "MyClass";
+
+void RedrawDisplay( HWND hwnd ){
+
+}
+
+LRESULT CALLBACK WindowProc( HWND hwnd,
+			     UINT msg,
+			     WPARAM wParam,
+			     LPARAM lParam ){
+
+}
+
+int WINAPI WinMain( HINSTANCE hInstance,
+		    HINSTANCE hPreInstance,
+		    LPSTR cmdLine,
+		    int cmdShow ){
+
+
+}
