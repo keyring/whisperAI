b3c1510df7ea23038181978df564e40fc9fdd81c
z.keyring <keyrings@163.com>
2013-06-02 16:38:27 +0800

    Add helper function..

--- src/WindowUtils.cpp
+++ src/WindowUtils.cpp
@@ -26,3 +26,82 @@ void CheckMenuItemAppropriatrly( HWND, hwnd, UINT menuItem, bool b ){
   else
     ChangeMenuState( hwnd, menuItem, MFS_UNCHECKED );
 }
+
+//-----------------------------------------------------------------------------
+//
+//  The following 3 functions are taken from Petzold's book and enable the 
+//  client to use the file dialog common control
+//-----------------------------------------------------------------------------
+void FileInitialize (HWND hwnd,
+                     OPENFILENAME& ofn,
+                     const std::string& defaultFileTypeDescription,
+                     const std::string& defaultFileExtension)
+{
+  std::string filter = defaultFileTypeDescription + '\0' + "*." + defaultFileExtension + '\0' +
+                       "All Files (*.*)" + '\0' + "*.*" + '\0' + '\0';
+  
+   static TCHAR szFilter[255];
+
+   for (unsigned int i=0; i<filter.size(); ++i)
+   {
+     szFilter[i] = filter.at(i);
+   }
+     
+     ofn.lStructSize       = sizeof (OPENFILENAME) ;
+     ofn.hwndOwner         = hwnd ;
+     ofn.hInstance         = NULL ;
+     ofn.lpstrFilter       = szFilter ;
+     ofn.lpstrCustomFilter = NULL ;
+     ofn.nMaxCustFilter    = 0 ;
+     ofn.nFilterIndex      = 0 ;
+     ofn.lpstrFile         = NULL ;          // Set in Open and Close functions
+     ofn.nMaxFile          = MAX_PATH ;
+     ofn.lpstrFileTitle    = NULL ;          // Set in Open and Close functions
+     ofn.nMaxFileTitle     = MAX_PATH ;
+     ofn.lpstrInitialDir   = NULL ;
+     ofn.lpstrTitle        = NULL ;
+     ofn.Flags             = 0 ;             // Set in Open and Close functions
+     ofn.nFileOffset       = 0 ;
+     ofn.nFileExtension    = 0 ;
+     ofn.lpstrDefExt       = defaultFileExtension.c_str() ;
+     ofn.lCustData         = 0L ;
+     ofn.lpfnHook          = NULL ;
+     ofn.lpTemplateName    = NULL ;
+
+}
+
+
+
+BOOL FileOpenDlg (HWND               hwnd,
+                  PTSTR              pstrFileName,
+                  PTSTR              pstrTitleName,
+                  const std::string& defaultFileTypeDescription,
+                  const std::string& defaultFileExtension)
+{
+     OPENFILENAME ofn;
+
+     FileInitialize(hwnd, ofn, defaultFileTypeDescription, defaultFileExtension);
+  
+     ofn.hwndOwner         = hwnd ;
+     ofn.lpstrFile         = pstrFileName ;
+     ofn.lpstrFileTitle    = pstrTitleName ;
+     ofn.Flags             = OFN_HIDEREADONLY | OFN_CREATEPROMPT ;
+     
+     return GetOpenFileName (&ofn) ;
+}
+
+BOOL FileSaveDlg (HWND               hwnd,
+                  PTSTR              pstrFileName,
+                  PTSTR              pstrTitleName,
+                  const std::string& defaultFileTypeDescription,
+                  const std::string& defaultFileExtension)
+{
+     OPENFILENAME ofn; FileInitialize(hwnd, ofn, defaultFileTypeDescription, defaultFileExtension);
+
+     ofn.hwndOwner         = hwnd ;
+     ofn.lpstrFile         = pstrFileName ;
+     ofn.lpstrFileTitle    = pstrTitleName ;
+     ofn.Flags             = OFN_OVERWRITEPROMPT ;
+     
+     return GetSaveFileName (&ofn) ;
+}
--- src/WindowUtils.h
+++ src/WindowUtils.h
@@ -22,4 +22,23 @@ inline void RedrawWindowRect( HWND hwnd, bool redrawBackground, RECT &redrawArea
   UpdateWindow(hwnd);
 }
 
+/* three handy functions from Mr Petzold. They open a common dialog box to
+grab a filename */
+void FileInitialize (HWND hwnd,
+                     OPENFILENAME& ofn,
+                     const std::string& defaultFileTypeDescription,
+                     const std::string& defaultFileExtension);
+
+BOOL FileOpenDlg (HWND               hwnd,
+                  PTSTR              pstrFileName,
+                  PTSTR              pstrTitleName,
+                  const std::string& defaultFileTypeDescription,
+                  const std::string& defaultFileExtension);
+
+BOOL FileSaveDlg (HWND hwnd,
+                  PTSTR pstrFileName,
+                  PTSTR pstrTitleName,
+                  const std::string& defaultFileTypeDescription,
+                  const std::string& defaultFileExtension);
+
 #endif	/* __WINDOWUTILS_H__ */
