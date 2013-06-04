ef191de48a731a5542c243609b37e762dda6c2ee
z.keyring <keyrings@163.com>
2013-06-04 09:46:00 +0800

    Add gdi helper class..

---
+++ src/wpgdi.h
@@ -0,0 +1,24 @@
+/****************************************************************************
+ *
+ *    wpgdi.h by z.keyring (c) 2013 All Rights Reserved.
+ *
+ *                                      --code with Emacs :)
+ *
+ *    DESC: Whisper Packaging GDI. A singleton class to help alleviate the 
+ *          tedium of using the GDI.Call each method using the #define for 
+ *          wpgdi-> .
+ ***************************************************************************/
+
+#ifndef __WPGDI_H__
+#define __WPGDI_H__
+
+#include <windows.h>
+#include <string>
+#include <vector>
+#include <cassert>
+
+#include "Vector2d.h"
+
+
+
+#endif	/* __WPGDI_H__ */
