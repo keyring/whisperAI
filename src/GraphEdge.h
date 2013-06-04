f613d88e9182ba31f2b04255f9134e0450f9d1b9
z.keyring <keyrings@163.com>
2013-06-04 16:15:15 +0800

    ...

--- src/GraphEdge.h
+++ src/GraphEdge.h
@@ -61,5 +61,48 @@ class GraphEdge{
   }
 };
 
+class NavGraphEdge : public GraphEdge{
+
+ public:
+  enum{
+    NORMAL = 0,
+    SWIM = 1 << 0,
+    CRAWL = 1 << 1,
+    CREEP = 1 << 2,
+    JUMP = 1 << 3,
+    FLY = 1 << 4,
+    GRAPPLE = 1 << 5,
+    GOES_THROUGH_DOOR = 1 << 6
+  };
+
+ protected:
+  int m_flags;
+  int m_IDofInterEntity;	/* if this edge intersects with an object( such as a door or lift), then this is that object's ID. */
+
+ public:
+
+ NavGraphEdge( int src, int dst, double cost, int flags = 0, int id = -1 ):
+  GraphEdge( src, dst, cost ), m_flags(flags), m_IDofInterEntity(id) {}
+
+  NavGraphEdge(std::ifstream &stream){
+    char buffer[50];
+    stream >> buffer >> m_src >> buffer >> m_dst >> buffer >> m_cost;
+    stream >> buffer >>m_flags >> buffer >>m_IDofInterEntity;
+  }
+
+  int GetFlags() const { return m_flags; }
+  void SetFlags(int value){ m_flags = value; }
+
+  int GetId() const { return m_IDofInterEntity; }
+  void SetId(int value){ m_IDofInterEntity = id; }
+
+  friend std::ostream &operator<<(std::ostream &os, const NavGraphEdge &n){
+    os << "m_src: " << n.m_src << "m_dst: " << n.m_dst
+       << "m_cost: " << n.m_cost << "m_flags: " << n.m_flags
+       << "ID: " << n.m_IDofInterEntity << std::endl;
+
+    return os;
+  }
+};
 
 #endif /* __GRAPHEDGE_H__ */
