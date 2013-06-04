b8134b84197740aba2c119051b1dc41d36e9b19c
z.keyring <keyrings@163.com>
2013-06-04 23:59:11 +0800

    ..

--- src/SparseGraph.h
+++ src/SparseGraph.h
@@ -22,6 +22,39 @@
 #include "Utils.h"
 
 
+template <class node_type, class edge_type>
+  class SparseGraph{
+
+ public:
+  typedef edge_type EdgeType;
+  typedef node_type NodeType;
+
+  typedef std::vector<node_type> NodeVector;
+  typedef std::list<edge_type> EdgeList;
+  typedef std::vector<EdgeList> EdgeListVector;
+
+ private:
+  NodeVector m_nodes;
+  EdgeListVector m_edges;
+  bool m_digraph;
+  int m_nextNodeIndex;
+
+  bool IsUniqueEdge( int src, int dst ) const;
+  void CullInvalidEdges();
+
+ public:
+ SparseGraph(bool digraph) : m_nextNodeIndex(0), m_digraph(digraph) {}
+
+  const NodeType &GetNode(int index) const;
+  NodeType &GetNode(int index);
+
+  const EdgeType &GetEdge(int src, int dst) const;
+  EdgeType &GetEdge(int src, int dst);
+
+  int GetNextFreeNodeIndex() const { return m_nextNodeIndex; }
+  int AddNode(node_type node);
+  void RemoveNode(int node);	/* remove by seting its index to invalidIndex */
+
 
 
 
