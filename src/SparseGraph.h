88740e2ab215b30291820bf98e5ff3fe3c7ae239
z.keyring <keyrings@163.com>
2013-06-04 23:45:10 +0800

    use sparse graph...

---
+++ src/SparseGraph.h
@@ -0,0 +1,29 @@
+/****************************************************************************
+ *
+ *    SparseGraph.h by z.keyring (c) 2013 All Rights Reserved.
+ *
+ *                                      --code with Emacs :)
+ *
+ *    DESC: Graph class using the adjacency list representation.
+ ***************************************************************************/
+
+#ifndef __SPARSEGRAPH_H__
+#define __SPARSEGRAPH_H__
+
+#pragma warning (disable:4786)
+
+#include <vector>
+#include <list>
+#include <cassert>
+#include <string>
+#include <iostream>
+
+#include "Vector2D.h"
+#include "Utils.h"
+
+
+
+
+
+
+#endif	/* __SPARSEGRAPH_H__ */
