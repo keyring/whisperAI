a8d9a3e94f8b607a43ff448997374cd23b9a37a5
z.keyring <keyrings@163.com>
2013-06-09 11:00:17 +0800

    ...

--- src/GraphAlgorithms.h
+++ src/GraphAlgorithms.h
@@ -13,6 +13,96 @@
 #ifndef __GRAPHALGORITHMS_H__
 #define __GRAPHALGORITHMS_H__
 
+#include <vector>
+#include <list>
+#include <queue>
+#include <stack>
+
+#include "SparseGraph"
+
+
+/* Graph_SearchDFS */
+
+template <class graph_type>
+class Graph_SearchDFS{
+
+
+
+};
+
+
+template <class graph_type>
+bool Graph_SearchDFS<graph_type>::Search(){
+
+}
+
+
+template <class graph_type>
+std::list<int> Graph_SearchDFS<graph_type>::GetPathToTarget() const {
+
+
+}
+
+
+/* Graph_SearchBFS */
+
+template <class graph_type>
+class Graph_SearchBFS{
+
+
+};
+
+template <class graph_type>
+bool Graph_SearchBFS<graph_type>::Search(){
+
+
+}
+
+template <class graph_type>
+std::list<int> Graph_SearchBFS<graph_type>::GetPathToTarget() const {
+
+}
+
+
+
+/* Graph_SearchDijkstra */
+
+template <class graph_type>
+class Graph_SearchDijkstra{
+
+
+};
+
+template <class graph_type>
+bool Graph_SearchDijkstra<graph_type>::Search(){
+
+}
+
+template <class graph_type>
+std::list<int> Graph_SearchDijkstra<graph_type>::GetPathToTarget() const {
+
+
+}
+
+
+/* Graph_SearchAStar */
+
+template <class graph_type>
+class Graph_SearchAStar{
+
+
+};
+
+template <class graph_type>
+bool Graph_SearchAStar<graph_type>::Search(){
+
+}
+
+template <class graph_type>
+std::list<int> Graph_SearchAStar<graph_type>::GetPathToTarget() const {
+
+}
+
 
 
 #endif	/* __GRAPHALGORITHMS_H__ */
