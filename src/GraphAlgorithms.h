3fa09288109b645aa7eb9197494321611b2e3192
z.keyring <keyrings@163.com>
2013-06-09 10:48:34 +0800

    Add GraphAlgorithms

---
+++ src/GraphAlgorithms.h
@@ -0,0 +1,18 @@
+/****************************************************************************
+ *
+ *    GraphAlgorithms.h by z.keyring (c) 2013 All Rights Reserved.
+ *
+ *                                      --code with Emacs :)
+ *
+ *    DESC: Classes to implement graph algorithms, include DFS, BFS,
+ *          Dijkstra, A* etc.
+ *          Any graphs passed to these functions must conform to the
+ *          same interface used by the SparseGraph.
+ ***************************************************************************/
+
+#ifndef __GRAPHALGORITHMS_H__
+#define __GRAPHALGORITHMS_H__
+
+
+
+#endif	/* __GRAPHALGORITHMS_H__ */
