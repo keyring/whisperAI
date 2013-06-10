22eddd979b06df94b7d21e7d555529a90a4829b5
z.keyring <keyrings@163.com>
2013-06-10 16:07:33 +0800

    Add new helper calss for search algorithms...

---
+++ src/AStarHeuristicPolicies.h
@@ -0,0 +1,49 @@
+/****************************************************************************
+ *
+ *    AStarHeuristicPolicies.h by z.keyring (c) 2013 All Rights Reserved.
+ *
+ *                                      --code with Emacs :)
+ *
+ *    DESC: Class templates defining a heuristic policy for use with
+ *          the A* search algorithm.
+ ***************************************************************************/
+
+#ifndef __ASTARHEURISTICPOLICIES_H__
+#define __ASTARHEURISTICPOLICIES_H__
+
+
+#include "Utils.h"
+
+class Heuristic_Euclid{
+ public:
+  Heuristic_Euclid(){ }
+
+template <class graph_type>
+  static double Calculate(const graph_type &g, int nd1, int nd2){
+
+  return Vec2DDistance(g.GetNode(nd1).GetPosition(), g.GetNode().GePosition());
+ }
+};
+
+class Heuristic_Noisy_Euclidian{
+ public:
+  Heuristic_Euclidian(){ }
+
+template <class graph_type>
+  static double Calculate(const graph_type &g, int nd1, int nd2){
+
+  return Vec2DDistance(g.GetNode(nd1).GetPosition(), g.GetNode().GePosition()) * RandInRange(0.9f, 1.1f);
+ }
+};
+
+class Heuristic_Dijkstra{
+  /* you can use this class to turn the A* into Dijkstra. */
+ public:
+template <class graph_type>
+  static double Calculate(const graph_type &g, int nd1, int nd2){
+  return 0;
+ }
+};
+
+
+#endif	/* __ASTARHEURISTICPOLICIES_H__ */
---
+++ src/PriorityQueue.h
@@ -0,0 +1,261 @@
+/****************************************************************************
+ *
+ *    PriorityQueue.h by z.keyring (c) 2013 All Rights Reserved.
+ *
+ *                                      --code with Emacs :)
+ ***************************************************************************/
+
+#ifndef __PRIORITYQUEUE_H__
+#define __PRIORITYQUEUE_H__
+
+#include <ostream>
+#include <vector>
+#include <cassert>
+
+template <class T>
+void Swap(T &a, T &b){
+  T temo = a;
+  a = b; 
+  b = temp;
+}
+
+template <class T>
+void RecorderUpwards(std::vector<T> &heap, int nd){
+
+  while((nd > 1) && (heap[nd/2] < heap[nd])){
+    Swap(heap[nd/2], heap[nd]);
+    nd /= 2;
+  }
+}
+
+template <class T>
+void RecorderDownwards(std::vector<T> &heap, int nd, int heapSize){
+
+  while(2 * nd <= heapSize){
+    int child = 2 * nd;
+
+    if((child < heapSize) && (heap[child] < heap[child+1])){
+      ++child;
+    }
+    if(heap[nd] < heap[child]){
+      Swap(heap[child], heap[nd]);
+      nd = child;
+    }
+    else
+      break;
+  }
+}
+
+
+/* PriorityQ
+ * basic heap based priority queue implementation.
+ */
+template <class T>
+class PriorityQ{
+
+ private:
+  std::vector<T> m_heap;
+  int m_size;
+  int m_maxSize;
+
+  void RecorderUpwards(std::vector<T> &heap, int nd){
+
+    while((nd>1) && (heap[nd/2] < heap[nd])){
+      Swap(heap[nd/2], heap[nd]);
+      nd /= 2;
+    }
+  }
+
+  void RecorderDownwards(std::vector<T> &heap, int nd, int heapSize){
+
+    while(nd*2 <= heapSize){
+      int child = nd * 2;
+
+      if((child <heapSize) && (heap[child] < heap[child+1])){
+	  ++child;
+	}
+
+	if(heap[nd] < heap[child]){
+	  Swap(heap[child], heap[nd]);
+	  nd = child;
+	}
+	else
+	  break;
+    }
+  }
+
+ public:
+ PriorityQ(int maxSize): m_maxSize(maxSize), m_size(0){
+
+    m_heap.assign(maxSize+1, T());
+  }
+
+  bool empty() const { return (m_size == 0); }
+
+  void insert(const T item){
+
+    assert(m_size+1 <= m_maxSize);
+
+    ++m_size;
+    m_heap[m_size] = item;
+    RecorderUpwards(m_heap, m_size);
+  }
+
+  T pop(){
+    Swap(m_heap[1], m_heap[m_size]);
+    RecorderDownwards(m_heap, 1, m_size-1);
+    return m_heap[m_size--];
+  }
+
+  const T &peek() const { return m_heap[1]; }
+};
+
+
+/* PriorityQLow
+ * basic 2-way heap based priority queue implementation. This time the
+ * priority is given to the lowest value key.
+ */
+template <class T>
+class PriorityQLow{
+
+ private:
+  std::vector<T> m_heap;
+  int m_size;
+  int m_maxSize;
+
+  void RecorderUpwards(std::vector<T> &heap, int nd){
+
+    while((nd>1) && (heap[nd/2] > heap[nd])){
+      Swap(heap[nd/2], heap[nd]);
+      nd /= 2;
+    }
+  }
+
+  void RecorderDownwards(std::vector<T> &heap, int nd, int heapSize){
+
+    while(nd *2 <= heapSize){
+      int child = 2 * nd;
+
+      if((child < heapSize) && (heap[child] > heap[child+1])){
+	++child;
+      }
+
+      if(heap[nd] > heap[child]){
+	Swap(heap[nd], heap[child]);
+	nd = child;
+      }
+      else
+	break;
+    }
+  }
+
+ public:
+ PriorityQLow(int maxSize) : m_maxSize(maxSize), m_size(0){
+    m_heap.assign(maxSize+1, T());
+  }
+
+  bool empty() const { return (m_size == 0); }
+
+  void insert(const T item){
+
+    assert(m_size+1 <= m_maxSize);
+    ++m_size;
+    m_heap[m_size] = item;
+    RecorderUpwards(m_heap, m_size);
+  }
+
+  T pop(){
+
+    Swap(m_heap[1], m_heap[m_size]);
+    RecorderDownwards(m_heap, 1, m_size-1);
+    return m_heap[m_size--];
+  }
+
+  const T &peek() const { return m_heap[1]; }
+};
+
+
+/* IndexedPriorityQLow
+ * Priority queue based on an index into a set of keys.
+ * The queue is maintained as a 2-way heap.
+ *
+ * The priority in this implementation is the lowest vaued key. */
+template <class T>
+class IndexdPriorityQLow{
+ private:
+  std::vector<T> &m_vecKeys;
+  std::vector<int> m_heap;
+  std::vector<int> m_invHeap;
+  int m_size;
+  int m_maxSize;
+
+  void Swap(int a, int b){
+
+    int temp = m_heap[a];
+    m_heap[a] = m_heap[b];
+    m_heap[b] = temp;
+
+    m_invHeap[m_heap[a]] = a;
+    m_invHeap[m_heap[b]] = b;
+  }
+
+  void RecorderUpwards(int nd){
+
+    while((nd>1) && (m_vecKeys[m_heap[nd/2]] > m_vecKeys[m_heap[nd]])){
+      Swap(nd/2, nd);
+      nd /= 2;
+    }
+  }
+
+  void RecorderDownwards(int nd, int heapSize){
+
+    while(nd*2 <= heapSize){
+      int child = nd * 2;
+
+      if((child < heapSize) && (m_vecKeys[m_heap[child]] > m_vecKeys[m_heap[child]])){
+	++child;
+      }
+      if(m_vecKeys[m_heap[nd]] > m_vecKeys[m_heap[child]]){
+	Swap(child, nd);
+	nd = child;
+      }
+      else
+	break;
+    }
+  }
+
+ public:
+ IndexdPriorityQLow(std::vector<T> &keys, int maxSize):
+  m_vecKeys(keys),
+    m_maxSize(maxSize),
+    m_size(0){
+
+    m_heap.assign(maxSize+1, 0);
+    m_invHeap.assign(maxSize+1, 0);
+  }
+
+  bool empty() const { return (m_size == 0); }
+
+  void insert(const int idx){
+
+    assert(m_size+1 <= m_maxSize);
+    ++m_size;
+    m_heap[m_size] = idx;
+    m_invHeap[idx] = m_size;
+    RecorderUpwards(m_size);
+  }
+
+  int pop(){
+
+    Swap(1, m_size);
+    RecorderDownwards(1, m_size-1);
+    return m_heap[m_size--];
+  }
+
+  void ChangePriority(const int idx){
+
+    RecorderUpwards(m_invHeap[idx]);
+  }
+};
+
+#endif	/* __PRIORITYQUEUE_H__  */
